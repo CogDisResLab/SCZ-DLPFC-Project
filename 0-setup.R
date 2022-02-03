4fdde59ea00859b0cc271ea6496c10b94e865448
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-03 18:41:43 -0500

    Added an ancillary data filder for intermediate files

--- 0-setup.R
+++ 0-setup.R
@@ -16,11 +16,12 @@ kea3_pair_data <- file.path(data_dir, "KEA3_Reports")
 region_data <- file.path(data_dir, "Region-Data")
 figures_dir <- "figures"
 depot_dir <- "depot"
+ancillary_dir <- "ancillary"
 
 
 all_dirs <- c(results_dir, data_dir, figures_dir, depot_dir,
               krsa_pair_data, kea3_pair_data, uka_pair_data,
-              region_data)
+              region_data, ancillary_dir)
 
 all_dirs |>
   walk(create_dir)
