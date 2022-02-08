d605b516b5dd1a83af31519380054f6456ce08ec
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-08 01:41:32 -0500

    Added additional directories

--- 0-setup.R
+++ 0-setup.R
@@ -15,6 +15,7 @@ krsa_pair_data <- file.path(data_dir, "KRSA_Reports")
 uka_pair_data <- file.path(data_dir, "UKA_Reports")
 kea3_pair_data <- file.path(data_dir, "KEA3_Reports")
 region_data <- file.path(data_dir, "Region-Data")
+pooled_pairwise_data <- file.path(data_dir, "Pooled-Data")
 figures_dir <- "figures"
 depot_dir <- "depot"
 ancillary_dir <- "ancillary"
@@ -23,7 +24,7 @@ creedenzymatic_figure_dir <- file.path(figures_dir, "creedenzymatic")
 
 all_dirs <- c(results_dir, data_dir, figures_dir, depot_dir,
               krsa_pair_data, kea3_pair_data, uka_pair_data,
-              region_data, ancillary_dir, creedenzymatic_figure_dir, creedenzymatic_results_dir)
+              region_data, ancillary_dir, creedenzymatic_figure_dir, creedenzymatic_results_dir, pooled_pairwise_data)
 
 all_dirs |>
   walk(create_dir)
