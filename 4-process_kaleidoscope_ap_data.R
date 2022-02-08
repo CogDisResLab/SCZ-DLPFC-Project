8ef12e0f41d6b8822580509c0919d1932720f1c1
Ali Sajid Imami <Ali.Sajid.Imami@gmail.com>
2022-02-08 00:15:32 -0500

    Added kaleidoscope gene lookup study

---
+++ 4-process_kaleidoscope_ap_data.R
@@ -0,0 +1,40 @@
+# Process the Kaleidoscope Antipsychotic Data
+
+library(tidyverse)
+
+raw_data <- readRDS("raw/Kaleidoscope-Data/ap_ks_datasets.rds")
+
+genes <- read_csv("ancillary/selected_kinase_gene_list.csv") |>
+  pull(hgnc_symbol)
+
+drug_full_name <- function(full_name) {
+  drugs = list(
+    "CLO" = "Clozapine",
+    "HAL" = "Haloperidol",
+    "Lox" = "Loxapine",
+    "ARI" = "Aripiprazole",
+    "QUE" = "Quetiapine",
+    "RIS" = "Risperidone",
+    "ZIP" = "Ziprasidone",
+    "CLZ" = "Clozapine",
+    "CL" = "Clozapine",
+    "LOX" = "Loxapine",
+    "OLA" = "Olanzapine",
+    "THI" = "Thiothixene",
+    "Valproate" = "Valproate",
+    "Lithium" = "Lithium"
+  )
+  short_name <-
+    full_name |>
+    str_split(pattern = "[\\W+_]", simplify = TRUE)
+  drugs[[short_name[[1]]]]
+}
+
+v_drug_name <- Vectorize(drug_full_name)
+
+clean_data <- raw_data |>
+  filter(HGNC_Symbol %in% genes) |>
+  select(-ecdfPlot) |>
+  mutate(drug = v_drug_name(DataSet)) |>
+  write_csv("results/antipsychotic_gene_lookups.csv")
+
