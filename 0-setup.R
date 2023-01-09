04ef16d13ec4b97655f38261141c78d22fa5cf82
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-01-09 13:55:19 -0500

    Added the additional data subdir

--- 0-setup.R
+++ 0-setup.R
@@ -18,6 +18,7 @@ region_data <- file.path(data_dir, "Region-Data")
 rats_data <- file.path(data_dir, "Rats-Data")
 pooled_pairwise_data <- file.path(data_dir, "Pooled-Data")
 kaleidoscope_data_dir <- file.path(data_dir, "kaleidoscope_data")
+selected_peptide_data_dir <- file.path(data_dir, "selected_peptide_data")
 figures_dir <- "figures"
 depot_dir <- "depot"
 ancillary_dir <- "ancillary"
@@ -28,7 +29,7 @@ reverse_krsa_figure_dir <- file.path(figures_dir, "reverse_krsa")
 all_dirs <- c(results_dir, data_dir, figures_dir, depot_dir,
               krsa_pair_data, kea3_pair_data, uka_pair_data,
               region_data, ancillary_dir, creedenzymatic_figure_dir, creedenzymatic_results_dir, pooled_pairwise_data, reverse_krsa_figure_dir,
-              kaleidoscope_data_dir, rats_data)
+              kaleidoscope_data_dir, rats_data, selected_peptide_data_dir)
 
 all_dirs |>
   walk(create_dir)
