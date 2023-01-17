d096b1f9647b21d632b6b3f0dd666be6c554c2ec
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-01-16 19:44:39 -0500

    Added an additional detailed report

--- 2-significant_peptide_exploration_threshold.R
+++ 2-significant_peptide_exploration_threshold.R
@@ -59,3 +59,30 @@ bottom_peptides <- all_datasets |>
   ) |>
   arrange(desc(Total)) |>
   write_csv("results/peptide_dn_selection_by_threshold_0.15.csv")
+
+all_peptides_signed <- all_datasets |>
+  pivot_longer(starts_with("R"), names_to = "Pair", values_to = "LogFC") |>
+  filter(abs(LogFC) > 0.15) |>
+  mutate(value = case_when(
+    LogFC <= -0.15 ~ -1,
+    LogFC >= 0.15 ~ 1
+  ),
+  Direction = if_else(value == -1, "DN", "UP")) |>
+  group_by(Peptide, Direction) |>
+  mutate(Total = sum(value, na.rm = TRUE)) |>
+  pivot_wider(names_from = Direction, values_from = Total) |>
+  ungroup() |>
+  select(-LogFC) |>
+  group_by(Peptide) |>
+  fill(UP, DN, .direction = "downup") |>
+  pivot_wider(names_from = Pair, values_fill = 0) |>
+  mutate(Total_Absolute = sum(abs(UP), abs(DN), na.rm = TRUE),
+         Total_Relative = sum(UP, DN, na.rm = TRUE)) |>
+  select(Peptide, starts_with("R"), Upregulated = UP, Downregulated = DN, Total_Absolute, Total_Relative) |>
+  mutate(across(starts_with("R"), ~ case_when(
+    .x == -1 ~ "Down",
+    .x == 1 ~ "Up",
+    .x == 0 ~ "Absent"
+  ))) |>
+  write_csv("results/peptide_selection_by_threshold_0.15-enhanced.csv")
+
