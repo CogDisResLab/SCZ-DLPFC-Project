7371dd390345a19e4b4c95f5ce78df370df21465
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-03-01 19:06:13 -0500

    Added SGK1 High Affinite peptides

--- 1-process_recombinant_kinase_peptides.R
+++ 1-process_recombinant_kinase_peptides.R
@@ -5,11 +5,21 @@ library(tidyverse)
 top_pep_files <-
   list.files(pattern = "*_top_pep[ts]\\w*.csv", recursive = TRUE)
 
+filter_top_hits <- function(df) {
+  if ("Cluster" %in% names(df)) {
+    df |>
+      filter(Cluster == "High Affinity")
+  } else {
+    df
+  }
+}
+
 process_top_peptides <- function(file) {
   gene_name <- str_extract(file, "(\\w*)_top") |>
     str_remove("_top")
   dataset <- read_csv(file) |>
-    select(-any_of(c("...1", "Threshold", "LFC Threshold"))) |>
+    filter_top_hits() |>
+    select(-any_of(c("...1", "Threshold", "LFC Threshold", "Cluster", "Type", "ID", "Gene"))) |>
     mutate(Source = gene_name)
 
   dataset
