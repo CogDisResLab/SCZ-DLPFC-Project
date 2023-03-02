dcd216c4ea030d8dbd13ec3f27b8bb9ef7f83bca
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-03-01 19:17:06 -0500

    Updated the recombinant plots with reverse krsa plots

--- f-visualize_pairwise_recombinant_peptide_data.R
+++ f-visualize_pairwise_recombinant_peptide_data.R
@@ -32,12 +32,48 @@ generate_heatmap <- function(dataset) {
   p
 }
 
+comparative_reverse_krsa <- function(dataset, kinase) {
+  g <- ggplot(dataset, aes(x = Pair, y = Score))
+
+  p <- g +
+    geom_boxplot() +
+    geom_jitter(width = 0.2, height = 0.2) +
+    theme_minimal() +
+    ggtitle(str_glue("Comparison of datasets for {kinase} family"),
+            str_glue("High Affinity Recombinant Peptides")) +
+    scale_y_continuous(
+      name = "Log_2 Fold Change",
+      limits = c(-2.5, 2.5),
+      breaks = seq(-2.5, 2.5, 0.5)
+    ) +
+    scale_x_discrete(name = "Dataset") +
+    theme(plot.title = element_text(hjust = 0.5),
+          plot.subtitle = element_text(hjust = 0.5))
+
+
+  p
+}
+
 peptide_data |>
   map(generate_heatmap) |>
   map2(
     kinases,
     ~ ggsave(
-      filename = str_glue("{.y}_recombinant_peptide_activity.png"),
+      filename = str_glue("{.y}_recombinant_peptide_activity_heatmap.png"),
+      plot = .x,
+      path = "figures/recombinant_peptide",
+      width = 12,
+      height = 8,
+      units = "in",
+      bg = "white"
+    )
+  )
+
+peptide_data |>
+  imap(~ comparative_reverse_krsa(.x, .y)) |>
+  imap(
+    ~ ggsave(
+      filename = str_glue("{.y}_recombinant_peptide_activity_boxplot.png"),
       plot = .x,
       path = "figures/recombinant_peptide",
       width = 12,
